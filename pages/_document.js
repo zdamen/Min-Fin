7b570a6d1d05631e210e60a3e251b52d0a4b9899
zdamen <zdamen@umich.edu>
2023-05-30 05:44:02 -0400

    added google analytics script tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <link href="//db.onlinewebfonts.com/c/875fffdfa62169a0f131e90f37f1faf4?family=Mazzard+H+Medium" rel="stylesheet" type="text/css"/>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"></link>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Tag Manager */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-9CZ6C2BYVQ"></script>
+          <script dangerouslySetInnerHTML={
+            {
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-9CZ6C2BYVQ');
+              `,
+            }
+          }>
+          </script>
+
+          {/* External Fonts */}
+          <link href="//db.onlinewebfonts.com/c/875fffdfa62169a0f131e90f37f1faf4?family=Mazzard+H+Medium" rel="stylesheet" type="text/css"/>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
